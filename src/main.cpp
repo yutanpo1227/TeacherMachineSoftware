d059594f8d058ece98d04e35614cc65e28fd3aa6
yutanpo1227 <komazawayuu1227@icloud.com>
2026-01-10 15:06:07 +0900

    update

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,19 @@
 #include "Debugger.h"
 
 // マシンのセットアップ値
-#define SPEED 200  // モーターの速度
+#define SPEED 100  // モーターの速度
 #define ENABLE_LINE_SENSOR true  // ラインセンサーの有効化
 #define ENABLE_GYRO_SENSOR true  // ジャイロセンサーの有効化
-#define ENABLE_DEBUGGER true  // デバッグモードの有効化
+#define ENABLE_DEBUGGER false  // デバッグモードの有効化
 
 // 回り込みの設定値
-#define WRAP_AROUND_ANGLE_THRESHOLD 10  // 回り込み角度の閾値
-#define WRAP_AROUND_BALL_DIST_THRESHOLD 850  // 回り込み距離の閾値
+#define WRAP_AROUND_ANGLE_THRESHOLD 5  // 回り込み角度の閾値
+#define WRAP_AROUND_BALL_DIST_THRESHOLD 900  // 回り込み距離の閾値
 #define WRAP_AROUND_ANGLE_RATIO 1.5  // 回り込み角度の倍率
+#define WRAP_AROUND_ANGLE 70  // 最大回り込み角度
 
 // ラインセンサーの閾値
-const int LINE_SENSOR_THRESHOLDS[] = {880, 850, 920, 870, 850, 850, 820, 900};
+const int LINE_SENSOR_THRESHOLDS[] = {860, 810, 890, 820, 800, 820, 820, 820};
 
 // センサーのインスタンス
 LineSensor lineSensor = LineSensor(0, 8, LINE_SENSOR_THRESHOLDS);
@@ -56,11 +57,11 @@ void loop() {
   const int gyroAngle = gyroSensor.readYawAngle();
   const int ballAngle = irSensor.readAngle();
   const int ballDist = irSensor.readDistance();
-
   debugger.printValues(gyroAngle, lineAngle, lineVectorMagnitude, ballAngle, ballDist);
 
   int moveAngle = calcWrapAroundAngle(ballAngle, ballDist);
-  motorController.moveDirection(moveAngle, SPEED, gyroAngle, lineAngle, lineVectorMagnitude);
+  // motorController.moveDirection(moveAngle, SPEED, gyroAngle, lineAngle, lineVectorMagnitude);
+  motorController.moveDirectionEight(moveAngle, SPEED, gyroAngle, lineAngle, lineVectorMagnitude);
 }
 
 
@@ -75,8 +76,13 @@ int calcWrapAroundAngle(int ballAngle, int ballDist) {
   int moveAngle = correctBallAngle;
   // ボールが近い時は大きく回り込む
   if (ballDist > WRAP_AROUND_BALL_DIST_THRESHOLD) {
-    if (correctBallAngle > WRAP_AROUND_ANGLE_THRESHOLD || correctBallAngle < -WRAP_AROUND_ANGLE_THRESHOLD) {
-      moveAngle = correctBallAngle * WRAP_AROUND_ANGLE_RATIO;
+    // if (correctBallAngle > WRAP_AROUND_ANGLE_THRESHOLD || correctBallAngle < -WRAP_AROUND_ANGLE_THRESHOLD) {
+    //   moveAngle = correctBallAngle * WRAP_AROUND_ANGLE_RATIO;
+    // }
+    if (correctBallAngle > WRAP_AROUND_ANGLE_THRESHOLD) {
+      moveAngle = correctBallAngle + 90;
+    } else if (correctBallAngle < -WRAP_AROUND_ANGLE_THRESHOLD) {
+      moveAngle = correctBallAngle - 90;
     }
   }
   // ボールの角度を元の座標系に変換
